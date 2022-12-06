4467581a504cbfba1e2516f52a3acc54971cebf9
Kenichi Takahama <hammer528@msn.com>
2022-12-06 09:44:32 +0900

    Project9_part1「Creating custom paths with SwiftUI」

--- Project9_part1/ContentView.swift
+++ Project9_part1/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    //カスタムパスを描画していきます
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+//            path.closeSubpath()
         }
-        .padding()
+        //strokeを使用してpathの周りを描画（別の修正方法）
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10,lineCap: .round,lineJoin: .round))
     }
 }
 
